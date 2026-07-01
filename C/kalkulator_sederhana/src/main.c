52c15ddaafd1d470eddd0c10fa82dd0c5e7b7890
viann <vyanpc940@gmail.com>
2026-07-01 15:13:07 +0700

    update main.c

--- C/kalkulator_sederhana/src/main.c
+++ C/kalkulator_sederhana/src/main.c
@@ -3,11 +3,15 @@
 #include <stdbool.h>
 
 int main(void){
-  printf("\t=====KALKULATOR SEDERHANA=====\n");
+  printf("=====KALKULATOR SEDERHANA=====\n");
   while (true){
 
     int pilih_operator; 
-    printf("\tpilih operator: ");scanf("%d", &pilih_operator);
+    printf("DAFTAR OPERATOR:\n");
+    printf("\n1. Penjumlahan\n2. Pengurangan\n3. Perkalian\n4. Pembagian\n0. Exit\n");
+    printf("<============================>\n");
+    printf("pilih operator: ");
+    scanf("%d", &pilih_operator);
 
     if (pilih_operator == 0){
       printf("\tbye...\n");
@@ -15,52 +19,52 @@ int main(void){
     }
 
     if (pilih_operator < 0 || pilih_operator > 4){
-      fprintf(stderr, "\tinput tidak valid!\n");
+      fprintf(stderr, "input tidak valid!\n");
       continue;
     }
     float angka1, angka2;
 
-    printf("\tmasukkan angka pertama: ");
+    printf("masukkan angka pertama: ");
     scanf("%f", &angka1);
 
-    printf("\tmasukkan angka kedua: ");
+    printf("masukkan angka kedua: ");
     scanf("%f", &angka2);
 
     switch (pilih_operator) {
       case 1:
-        printf("\t<=========================>\n");
-        printf("\thasil penjumlahan: %.2f\n", tambah(angka1, angka2));
-        printf("\t<=========================>\n");
+        printf("<=========================>\n");
+        printf("hasil penjumlahan: %.2f\n", tambah(angka1, angka2));
+        printf("<=========================>\n");
 
         break;
 
       case 2:
-        printf("\t<=========================>\n");
-        printf("\thasil pengurangan: %.2f\n", kurang(angka1, angka2));
-        printf("\t<=========================>\n");
+        printf("<=========================>\n");
+        printf("hasil pengurangan: %.2f\n", kurang(angka1, angka2));
+        printf("<=========================>\n");
 
         break;
 
       case 3:
-        printf("\t<=========================>\n");
-        printf("\thasil perkalian: %.2f\n", kali(angka1, angka2));
-        printf("\t<=========================>\n");
+        printf("<=========================>\n");
+        printf("hasil perkalian: %.2f\n", kali(angka1, angka2));
+        printf("<=========================>\n");
 
         break;
 
       case 4:
         if (angka2 == 0){
-          fprintf(stderr, "\tangka kedua != 0!\n");
+          fprintf(stderr, "angka kedua != 0!\n");
           continue;
         }
-        printf("\t<=========================>\n");
-        printf("\thasil pembagian: %.2f\n", bagi(angka1, angka2));
-        printf("\t<=========================>\n");
+        printf("<=========================>\n");
+        printf("hasil pembagian: %.2f\n", bagi(angka1, angka2));
+        printf("<=========================>\n");
 
         break;
 
       default:
-        printf("\tinput tidak valid!\n");
+        printf("input tidak valid!\n");
 
     }
 
